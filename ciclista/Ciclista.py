52d216a9f8e7065ad105aa4b34baa79fad1d47cb
206 <206@academicos.cesde.edu.co>
2022-05-06 19:36:31 -0500

    Hechos los trabajos

---
+++ ciclista/Ciclista.py
@@ -0,0 +1,36 @@
+
+class Ciclista:
+
+    def __init__(self):
+        self.__nombre=None
+        self.__edad=None
+        self.__pais=None
+        self.__tiempo=None
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+    @property
+    def pais(self):
+        return self.__pais
+    @property
+    def edad(self):
+        return self.__edad
+    @property
+    def tiempo(self):
+        return self.__tiempo
+    
+
+    @nombre.setter
+    def nombre(self,nombre):
+        self.__nombre=nombre
+    @edad.setter
+    def edad(self,edad):
+        self.__edad=edad
+    @pais.setter
+    def pais(self,pais):
+        self.__pais=pais
+    @tiempo.setter
+    def tiempo(self,tiempo):   
+            self.__tiempo=tiempo
+        
